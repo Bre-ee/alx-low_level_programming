d618d46089a7d5e60fb080827b6805f44ec2b085
bre-ee <bnyakio52@gmail.com>
2022-07-09 02:15:59 -0700

    learning to print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - prints Programming is like building a multilingual puzzle.
  *
- * Return - Always O
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
